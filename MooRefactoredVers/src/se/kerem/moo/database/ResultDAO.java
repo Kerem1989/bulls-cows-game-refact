5347f1001ff4f03710683a55b6520dcfa731cba0
Kerem Bjälvenäs Tazedal <98690898+Kerem1989@users.noreply.github.com>
2024-01-28 21:21:53 +0100

    Delete MooRefactoredVers directory

--- MooRefactoredVers/src/se/kerem/moo/database/ResultDAO.java
+++
@@ -1,8 +0,0 @@
-package se.kerem.moo.database;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public interface ResultDAO {
-    void insertUserChoice (int nGuess, int id) throws SQLException;
-    ResultSet selectResultByPlayerId (int id) throws SQLException;
-}
--- MooRefactoredVers/src/se/kerem/moo/io/IO.java
+++
@@ -1,9 +0,0 @@
-package se.kerem.moo.io;
-
-public interface IO {
-    boolean yesNo(String prompt);
-    String getString();
-    void addString(String s);
-    void clear();
-    void exit();
-}
